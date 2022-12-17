b204d4e4bc63df114aa582ea90eb6e8e2021fe38
Veronika <jackpot7815RUS@mail.ru>
2022-12-17 13:44:39 +0300

    Cоздали поле для ходьбы кораблей.

--- ProjectSeaBattle/FormField.Designer.cs
+++ ProjectSeaBattle/FormField.Designer.cs
@@ -29,12 +29,26 @@ namespace ProjectSeaBattle
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dgvField1 = new System.Windows.Forms.DataGridView();
             this.buttonToRun = new System.Windows.Forms.Button();
             this.buttonRestart = new System.Windows.Forms.Button();
             this.dgvField2 = new System.Windows.Forms.DataGridView();
+            this.dgvMove = new System.Windows.Forms.DataGridView();
+            this.dataGridViewImageColumn1 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.dataGridViewImageColumn2 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.dataGridViewImageColumn3 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.dataGridViewImageColumn4 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.buttonUp = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvField1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvField2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMove)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvField1
@@ -77,11 +91,121 @@ namespace ProjectSeaBattle
             this.dgvField2.Size = new System.Drawing.Size(183, 183);
             this.dgvField2.TabIndex = 34;
             // 
+            // dgvMove
+            // 
+            this.dgvMove.AllowUserToAddRows = false;
+            this.dgvMove.AllowUserToDeleteRows = false;
+            this.dgvMove.AllowUserToResizeColumns = false;
+            this.dgvMove.AllowUserToResizeRows = false;
+            this.dgvMove.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMove.ColumnHeadersVisible = false;
+            this.dgvMove.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewImageColumn1,
+            this.dataGridViewImageColumn2,
+            this.dataGridViewImageColumn3,
+            this.dataGridViewImageColumn4});
+            this.dgvMove.Location = new System.Drawing.Point(607, 59);
+            this.dgvMove.Name = "dgvMove";
+            this.dgvMove.ReadOnly = true;
+            this.dgvMove.RowHeadersVisible = false;
+            this.dgvMove.RowTemplate.Height = 35;
+            this.dgvMove.Size = new System.Drawing.Size(143, 283);
+            this.dgvMove.TabIndex = 35;
+            // 
+            // dataGridViewImageColumn1
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.NullValue = null;
+            this.dataGridViewImageColumn1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridViewImageColumn1.HeaderText = "Column1";
+            this.dataGridViewImageColumn1.Name = "dataGridViewImageColumn1";
+            this.dataGridViewImageColumn1.ReadOnly = true;
+            this.dataGridViewImageColumn1.Width = 35;
+            // 
+            // dataGridViewImageColumn2
+            // 
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.NullValue = null;
+            this.dataGridViewImageColumn2.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridViewImageColumn2.HeaderText = "Column2";
+            this.dataGridViewImageColumn2.Name = "dataGridViewImageColumn2";
+            this.dataGridViewImageColumn2.ReadOnly = true;
+            this.dataGridViewImageColumn2.Width = 35;
+            // 
+            // dataGridViewImageColumn3
+            // 
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.NullValue = null;
+            this.dataGridViewImageColumn3.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridViewImageColumn3.HeaderText = "Column3";
+            this.dataGridViewImageColumn3.Name = "dataGridViewImageColumn3";
+            this.dataGridViewImageColumn3.ReadOnly = true;
+            this.dataGridViewImageColumn3.Width = 35;
+            // 
+            // dataGridViewImageColumn4
+            // 
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.NullValue = null;
+            this.dataGridViewImageColumn4.DefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridViewImageColumn4.HeaderText = "Column4";
+            this.dataGridViewImageColumn4.Name = "dataGridViewImageColumn4";
+            this.dataGridViewImageColumn4.ReadOnly = true;
+            this.dataGridViewImageColumn4.Width = 35;
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.BackColor = System.Drawing.Color.White;
+            this.buttonLeft.Image = global::ProjectSeaBattle.Properties.Resources.left;
+            this.buttonLeft.Location = new System.Drawing.Point(725, 348);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(40, 40);
+            this.buttonLeft.TabIndex = 40;
+            this.buttonLeft.Tag = "1";
+            this.buttonLeft.UseVisualStyleBackColor = false;
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.BackColor = System.Drawing.Color.White;
+            this.buttonRight.Image = global::ProjectSeaBattle.Properties.Resources.right;
+            this.buttonRight.Location = new System.Drawing.Point(679, 348);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(40, 40);
+            this.buttonRight.TabIndex = 39;
+            this.buttonRight.Tag = "1";
+            this.buttonRight.UseVisualStyleBackColor = false;
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.BackColor = System.Drawing.Color.White;
+            this.buttonDown.Image = global::ProjectSeaBattle.Properties.Resources.down;
+            this.buttonDown.Location = new System.Drawing.Point(633, 348);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(40, 40);
+            this.buttonDown.TabIndex = 38;
+            this.buttonDown.Tag = "1";
+            this.buttonDown.UseVisualStyleBackColor = false;
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.BackColor = System.Drawing.Color.White;
+            this.buttonUp.Image = global::ProjectSeaBattle.Properties.Resources.up;
+            this.buttonUp.Location = new System.Drawing.Point(587, 348);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(40, 40);
+            this.buttonUp.TabIndex = 36;
+            this.buttonUp.Tag = "1";
+            this.buttonUp.UseVisualStyleBackColor = false;
+            // 
             // FormField
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.dgvMove);
             this.Controls.Add(this.dgvField2);
             this.Controls.Add(this.buttonRestart);
             this.Controls.Add(this.buttonToRun);
@@ -90,6 +214,7 @@ namespace ProjectSeaBattle
             this.Text = "FormField";
             ((System.ComponentModel.ISupportInitialize)(this.dgvField1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvField2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMove)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -100,5 +225,14 @@ namespace ProjectSeaBattle
         private System.Windows.Forms.Button buttonToRun;
         private System.Windows.Forms.Button buttonRestart;
         private System.Windows.Forms.DataGridView dgvField2;
+        private System.Windows.Forms.DataGridView dgvMove;
+        private System.Windows.Forms.DataGridViewImageColumn dataGridViewImageColumn1;
+        private System.Windows.Forms.DataGridViewImageColumn dataGridViewImageColumn2;
+        private System.Windows.Forms.DataGridViewImageColumn dataGridViewImageColumn3;
+        private System.Windows.Forms.DataGridViewImageColumn dataGridViewImageColumn4;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttonLeft;
     }
 }
